8eb86246b11641990443277e55944d0e908093cc
KalashSuman <116900176+KalashSuman@users.noreply.github.com>
2022-11-07 14:58:50 +0530

    Add files via upload

---
+++ TABLE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ int a,i;
+clrscr();h
+  printf("Enter the number");
+  scanf ("%d", &a);
+  for(i=1;i<=10;i++)
+  { printf ("\n%d", i*a);
+  }
+  getch();
+}
